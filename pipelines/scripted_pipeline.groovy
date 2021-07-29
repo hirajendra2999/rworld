631b0dcc7cc90ca58dd1c5f8337ba0d1820c82ad
admin <rxhelpline999@gmail.com>
2021-07-29 06:47:32 +0530

    --- Auto-Git Commit ---

--- pipelines/scripted_pipeline.groovy
+++ pipelines/scripted_pipeline.groovy
@@ -1,9 +1,10 @@
 node {  
     stage('Pull') { 
-        // 
+        git credentialsId: 'github-git', url: 'https://github.com/hirajendra2999/rworld.git'     
     }
     stage('Test') { 
-        // 
+        echo
+    
     }
     stage('Deploy') { 
         // 
