0adc6a4b99e36f68ab65192a25137602c3597889
admin <rxhelpline999@gmail.com>
2021-07-31 07:00:27 +0530

    --- Auto-Git Commit ---

---
+++ pipelines/seedjobs/pipelineTemplate.groovy
@@ -0,0 +1,77 @@
+pipelineJob('pipeline_demo') {
+
+  description('')
+
+  displayName('pipelinejob_converted')
+
+  keepDependencies(false)
+
+  quietPeriod(0)
+
+  checkoutRetryCount(0)
+
+  disabled(false)
+
+  concurrentBuild(false)
+
+  configure { flowdefinition ->
+
+    flowdefinition / 'actions' << 'org.jenkinsci.plugins.pipeline.modeldefinition.actions.DeclarativeJobAction'(plugin:'pipeline-model-definition')
+
+    flowdefinition / 'actions' << 'org.jenkinsci.plugins.pipeline.modeldefinition.actions.DeclarativeJobPropertyTrackerAction'(plugin:'pipeline-model-definition') {
+
+      'jobProperties'()
+
+      'triggers'()
+
+      'parameters'()
+
+      'options'()
+
+    }
+
+    flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
+
+      'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
+
+        'configVersion'(2)
+
+        'userRemoteConfigs' {
+
+          'hudson.plugins.git.UserRemoteConfig' {
+
+            'url'('https://github.com/hirajendra2999/rworld.git')
+
+            'credentialsId'('github-git')
+
+          }
+
+        }
+
+        'branches' {
+
+          'hudson.plugins.git.BranchSpec' {
+
+            'name'('*/master')
+
+          }
+
+        }
+
+        'doGenerateSubmoduleConfigurations'(false)
+
+        'submoduleCfg'(class:'empty-list')
+
+        'extensions'()
+
+      }
+
+      'scriptPath'('pipelines/pipeline2.jdp')
+
+      'lightweight'(true)
+
+    }
+
+  }
+
+}
